0111e590ccce154cbf69e6b7006b5664720b0471
ronnie <boluoadai@163.com>
2017-08-22 14:52:29 +0800

    xx

---
+++ src/components/header.vue
@@ -0,0 +1,21 @@
+<template>
+  <mt-header fixed title="豆瓣app">
+    <router-link to="/" slot="left">
+      <mt-button icon="back">返回</mt-button>
+    </router-link>
+    <mt-button slot="right">分享</mt-button>
+  </mt-header>
+</template>
+<style scoped>
+  header{
+    background-color: #42bd56;
+  }
+</style>
+<script>
+  export default {
+    data () {
+      return {
+      }
+    }
+  }
+</script>
